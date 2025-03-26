19711a752338bc0fd0a2934a998b4300b822610a
tranzillo <tranzillo.m@gmail.com>
2025-03-26 15:43:34 -0400

    added build props to config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,11 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import disciplineColorsIntegration from './src/integrations/disciplineColors';
+import node from '@astrojs/node';
 
 export default defineConfig({
   integrations: [react(), disciplineColorsIntegration(),],
-  // Enable SSR for dynamic routes
-  output: 'static',
-  // Use SCSS for styling
+
   vite: {
     css: {
       preprocessorOptions: {
@@ -15,5 +14,15 @@ export default defineConfig({
         }
       }
     }
+  },
+
+  output: 'static',
+  adapter: node({
+    mode: 'standalone'
+  }),
+
+  build: {
+    client: '../',
+    server: './server'
   }
 });
